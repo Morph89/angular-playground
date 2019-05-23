40965cf63936ecf4821abedb657448ed30de0a55
Morph89 <mate.kertesz@icloud.com>
2019-05-23 13:23:14 +0200

    1 file modified, 2 added

---
+++ src/app/store/reducers/flights.reducers.ts
@@ -0,0 +1,9 @@
+import { Action } from '@ngrx/store';
+import { ActionTypes } from '../actiontypes/flight.actions';
+
+const initialState = null;
+export flightReducer(state = initialState, action: Action) {
+  switch(action) {
+
+  }
+}
